1f3d96e13f79d65371c0bdcbc678a2a8abd46972
sealfury <willspecker@gmail.com>
2022-01-22 01:57:56 +0100

    adds editor configuration options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,24 @@
 import Editor from '@monaco-editor/react'
 
 const CodeEditor = () => {
-  return <Editor height='500px' />
+  return (
+    <Editor
+      language='javascript'
+      theme='dark'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        fontFamily: 'Arial',
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  )
 }
 
 export default CodeEditor
