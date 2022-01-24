b5ca4ced2fd0c10fcddf91401da790acb8aae222
sealfury <willspecker@gmail.com>
2022-01-24 17:20:54 +0100

    adds logic to add multiple code/preview windows, creates CodeCell wrapper component for Editor & Preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
 import 'bulmaswatch/solar/bulmaswatch.min.css'
-import { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-import { CodeEditor, Preview } from './components'
-import { bundle } from './bundler'
+import { CodeCell } from './components'
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
-
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output)
-  }
-
   return (
     <div>
-      <CodeEditor
-        initialValue='// Start writing some code!'
-        onChange={val => setInput(val)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   )
 }
