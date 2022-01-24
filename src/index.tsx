1e81184eb9907ba747c71efbca2853070458f742
sealfury <willspecker@gmail.com>
2022-01-24 16:25:08 +0100

    extracts bundling logic to master bundling fn

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,17 @@
 import 'bulmaswatch/solar/bulmaswatch.min.css'
-import * as esbuild from 'esbuild-wasm'
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-import { unpkgPathPlugin, fetchPlugin } from './plugins'
 import { CodeEditor, Preview } from './components'
+import bundle from './bundler'
 
 const App = () => {
-  const ref = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      // externally fetch web assembly binary
-      // return to local usage from public folder
-      // if necessary - see PR #1
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
-  useEffect(() => {
-    startService()
-  }, [])
-
   const onClick = async () => {
-    if (!ref.current) {
-      return
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        // aka str 'production', not var named production
-        'process.env.NODE_ENV': '"production"',
-        // replaces var global with window for browser (webpack does automatically)
-        global: 'window',
-      },
-    })
-
-    setCode(result.outputFiles[0].text)
+    const output = await bundle(input)
+    setCode(output)
   }
 
   return (
