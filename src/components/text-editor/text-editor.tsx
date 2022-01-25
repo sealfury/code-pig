d886987a3b05bf944cf32b4f65e43f293c7313fe
sealfury <willspecker@gmail.com>
2022-01-25 20:47:35 +0100

    implements conditional toggling of edit/preview mode of md editor on click

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -1,8 +1,42 @@
+import { useState, useEffect, useRef } from 'react'
 import MDEditor from '@uiw/react-md-editor'
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const [isEditing, setIsEditing] = useState(false)
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log('element clicked is inside editor')
+        return
+      }
+
+      console.log('element clicked is NOT inside editor')
+
+      setIsEditing(false)
+    }
+    document.addEventListener('click', listener, { capture: true })
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
+
+  if (isEditing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div onClick={() => setIsEditing(true)}>
       <MDEditor.Markdown source={'# Header'} />
     </div>
   )
