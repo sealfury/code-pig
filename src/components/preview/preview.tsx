d12faf980a97aab8da3e6ea60e6dcbd46d94eba2
sealfury <willspecker@gmail.com>
2022-01-25 01:22:43 +0100

    moves bgnd styling to iframe html to prevent preview window jumps

--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -7,7 +7,9 @@ interface PreviewProps {
 
 const html = /*html*/ `
     <html>
-      <head></head>
+      <head>
+        <style>html { background-color: #e0ffea; };</style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
