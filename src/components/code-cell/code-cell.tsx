1e6db3f69ad19e13c5f35a62c453e112f879ab11
sealfury <willspecker@gmail.com>
2022-01-27 00:16:35 +0100

    extracts local state from code cell to redux

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from 'react'
 import { CodeEditor, Preview, Resizable } from '..'
 import { bundle } from '../../bundler'
+import { Cell } from '../../state'
+import { useActions } from '../../hooks/use-actions'
 
-const CodeCell = () => {
-  const [input, setInput] = useState('')
+interface CodeCellProps {
+  cell: Cell
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('')
   const [err, setErr] = useState('')
+  const { updateCell } = useActions()
 
   // auto-bundle user code after 0.8s
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input)
+      const output = await bundle(cell.content)
       setCode(output.code)
       setErr(output.err)
     }, 800)
@@ -18,15 +24,15 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer)
     }
-  }, [input])
+  }, [cell.content])
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
           <CodeEditor
-            initialValue='// Start writing some code!'
-            onChange={val => setInput(val)}
+            initialValue={cell.content}
+            onChange={val => updateCell(cell.id, val)}
           />
         </Resizable>
         <Preview code={code} bundleErr={err} />
