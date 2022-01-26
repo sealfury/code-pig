a7b1a0afe9a82cfa8c585e386ecc5e2b4da80234
sealfury <willspecker@gmail.com>
2022-01-26 18:59:53 +0100

    scaffolds and connects cell list / list item components

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import { store } from './state'
-import { CodeCell, TextEditor } from './components'
+import { CellList } from './components'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CodeCell />
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   )
