4464468b8c75884bb5d908cfff36fca8deb480f3
sealfury <willspecker@gmail.com>
2022-01-27 20:05:52 +0100

    refactors components to implement insert cell after action

--- src/components/add-cell/add-cell-button.tsx
+++ src/components/add-cell/add-cell-button.tsx
@@ -5,7 +5,10 @@ interface AddCellButtonProps {
 
 const AddCellButton: React.FC<AddCellButtonProps> = ({ onClick, type }) => {
   return (
-    <button className='button is-rounded is-primary is-small' onClick={onClick}>
+    <button
+      className='button is-rounded is-secondary is-small'
+      onClick={onClick}
+    >
       <span className='icon is-small'>
         <i className='fas fa-plus' />
       </span>
