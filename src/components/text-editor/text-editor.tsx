c4cf303f85bd6d66d40f9b8b21ffa5394b98d80b
sealfury <willspecker@gmail.com>
2022-01-27 12:23:33 +0100

    extracts local state from text cell

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -1,11 +1,17 @@
 import './text-editor.css'
 import { useState, useEffect, useRef } from 'react'
 import MDEditor from '@uiw/react-md-editor'
+import { Cell } from '../../state'
+import { useActions } from '../../hooks/use-actions'
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+  cell: Cell
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null)
   const [isEditing, setIsEditing] = useState(false)
-  const [value, setValue] = useState('# Header')
+  const { updateCell } = useActions()
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -32,7 +38,10 @@ const TextEditor: React.FC = () => {
   if (isEditing) {
     return (
       <div className='text-editor' ref={ref}>
-        <MDEditor value={value} onChange={v => setValue(v || '')} />
+        <MDEditor
+          value={cell.content}
+          onChange={v => updateCell(cell.id, v || '')}
+        />
       </div>
     )
   }
@@ -40,7 +49,7 @@ const TextEditor: React.FC = () => {
   return (
     <div className='text-editor card' onClick={() => setIsEditing(true)}>
       <div className='card-content'>
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'Click here to edit!'} />
       </div>
     </div>
   )
