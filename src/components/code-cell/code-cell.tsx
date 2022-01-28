7a1885bd61d78ab587a39b0f30d68c2513b07adb
sealfury <willspecker@gmail.com>
2022-01-28 01:14:38 +0100

    scaffolds eager bundling functionality and clears dep warning

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -12,7 +12,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector(state => state.bundles[cell.id])
 
   // auto-bundle user code after 0.8s
+  // if there is no bundle, immediately create it
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content)
+      return
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content)
     }, 800)
@@ -20,6 +26,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle])
 
   return (
