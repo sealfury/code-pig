9fadb94a2d860093455eb3e98ce8adee41276c12
sealfury <willspecker@gmail.com>
2022-01-29 14:36:50 +0100

    expands show fn to handle jsx & import statements

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -17,11 +17,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const orderedCells = order.map(id => data[id])
     const cumulativeCode = [
       `
-        const show = (value) => {
-          if (typeof value === 'object') {
-            document.querySelector('#root).innerHTML = JSON.stringify(value)
+        import _React from 'react'
+        import _ReactDOM from 'react-dom'
+        
+        var show = (value) => {
+          const root = document.querySelector('#root')
+          if (typeof  value === 'object') {
+            if (value.$$typeof && value.props) {
+              _ReactDOM.render(value, root)
+            } else {
+              root.innerHTML = JSON.stringify(value)
+            }
           } else {
-            document.querySelector('#root).innerHTML = value
+            root.innerHTML = value
           }
         }
       `,
