aba22c2485ef352fc767307449eaf55b1685c1fa
sealfury <willspecker@gmail.com>
2022-02-13 13:56:11 +0100

    improves error messages on failure of serve command

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -7,8 +7,23 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file to edit')
   .option('-p, --port <number>', 'Port to run server', '4005')
-  .action((filename = 'notes.js', options: { port: string }) => {
-    // access file whether user provides exact path or not
-    const dir = path.join(process.cwd(), path.dirname(filename))
-    serve(parseInt(options.port), path.basename(filename), dir)
+  .action(async (filename = 'notes.js', options: { port: string }) => {
+    try {
+      // access file whether user provides exact path or not
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      await serve(parseInt(options.port), path.basename(filename), dir)
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      )
+    } catch (err: any) {
+      if (err.code === 'EADDRINUSE') {
+        console.error(
+          `Port is in use.  Try running on a different port.
+          Hint: use -p flag to choose port, or run 'serve --help' for options`
+        )
+      } else {
+        console.log('A problem occurred:', err.message)
+      }
+      process.exit(1) // force exit with unsuccessful run of program
+    }
   })
