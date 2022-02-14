781d2522ba6262a1362a129b056299517372a063
sealfury <willspecker@gmail.com>
2022-02-14 16:40:12 +0100

    client - complete and debounce save cell logic

--- packages/local-client/src/state/middlewares/persist-middleware.ts
+++ packages/local-client/src/state/middlewares/persist-middleware.ts
@@ -2,12 +2,17 @@ import { Dispatch } from 'redux'
 import { Action } from '../actions'
 import { ActionType } from '../action-types'
 import { saveCells } from '../action-creators'
+import { RootState } from '../reducers'
 
 export const persistCellData = ({
   dispatch,
+  getState,
 }: {
   dispatch: Dispatch<Action>
+  getState: () => RootState
 }) => {
+  let timer: any
+
   return (next: (action: Action) => void) => {
     return (action: Action) => {
       next(action)
@@ -20,7 +25,13 @@ export const persistCellData = ({
           ActionType.DELETE_CELL,
         ].includes(action.type)
       ) {
-        console.log('trying to save cells...')
+        if (timer) {
+          clearTimeout(timer)
+        }
+        // save cell content after 0.3s pause
+        timer = setTimeout(() => {
+          saveCells()(dispatch, getState)
+        }, 300)
       }
     }
   }
